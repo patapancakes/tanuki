8c02a6308b4a9de8621fbc3ae0fe9d4ea56b877e
Pancakes <patapancakes@pagefault.games>
2026-04-12 21:43:42 -0400

    Add Expires header in cache function

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	. "github.com/patapancakes/tanuki/config"
 	"github.com/patapancakes/tanuki/pages"
@@ -86,6 +87,7 @@ func cache(h http.Handler) http.HandlerFunc {
 			return
 		}
 
+		w.Header().Set("Expires", time.Now().UTC().Add(time.Hour*24*7).Format(time.RFC1123))
 		w.Header().Set("Cache-Control", "public, max-age=604800, immutable")
 		h.ServeHTTP(w, r)
 	}
